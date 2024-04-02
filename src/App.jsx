fbb91bd1a1641b724b49898b47d9f56e6af2a873
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-04-02 11:25:28 +0530

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@material-tailwind/react";
+ 
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+   <Button>Button</Button>
+
+    </>
+  );
+}
+
+export default App;
