bd08bade335e9abe1392fd90f1aaa95b8ec6a03e
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-04-02 19:26:04 +0530

    changed colours

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,7 +3,7 @@ import { NavbarWithSearch } from "../components/Navbar";
 
 export default function RootLayout(){
     return(
-    <div className="bg-gray-900 absolute w-[100%] h-screen">
+    <div className="bg-black absolute w-[100%] h-screen">
         <div className="mb-3"><NavbarWithSearch/></div>
         <Outlet/>
     </div>)
